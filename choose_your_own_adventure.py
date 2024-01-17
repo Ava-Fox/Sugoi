170fe56d9498d831b61db0b438b0eabbc594b623
avafox <avafoxemail@gmail.com>
2024-01-16 20:56:37 -0700

    Develop more of the choose your own adventure script

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -3,23 +3,38 @@ import story_script as ss
 
 #Prompt user if wanna play/what your name is 
 print(textwrap.fill(ss.intro))
-your_name = input('''\n"What's your name, hotshot?"\n> ''')
+name = input('''\n"What's your name, hotshot?"\n> ''').title()
 
 invite_to_practice = f'''
-    "I like your stuff, {your_name.title()}. What do you think about coming over to my band's next practice to jam?"
+    "I like your stuff, {name.title()}. What do you think about coming over to my band's next practice to jam?"
     ''' #An explination of what happens next, he invites you over to meet the band, you choose yes or no
 
 print(textwrap.dedent(invite_to_practice))
 print(textwrap.fill(ss.realization))
     #starting game???
 
-prompt = "\n> do you wanna go? (yes/no) "
+prompt = "\n> Do you wanna go? (yes/no) "
 answer = input(prompt)
 
 if answer != "no":
     #He takes you to band practice
     band_practice = "\nA section explaining what band practice is like and meeting secondary characters and explaining how hot they are"
-    print(band_practice)
+    print(textwrap.fill(ss.band_practice))
+
+    instrument = input("\n> What do you play? ")
+    andy_meet = f'''
+    'Ah, the {instrument}! What lovely tunes the {instrument} produces!', he says while staring directly into your soul. 'I don't believe we've met before, I'm Andy." You've definitely met before, but you tell him your name anyways.
+'''
+    print(textwrap.fill(andy_meet))
+
+    print(textwrap.fill(ss.andy_description))
+
+    griffin_buts_in = f'''
+    'I told {name} she could come to practice today, heard her in the practice rooms - she's pretty good.' Griffin says, giving you a chance to take a carefully measured inhale and survey your surroundings. 
+'''
+    print(textwrap.fill(griffin_buts_in))
+    print(textwrap.fill(ss.house_description))
+
         
 else:
    #End game
--- story_script.py
+++ story_script.py
@@ -1,14 +1,27 @@
 
 #This is where I will keep the paragraphs for the story so I can import them into the main program
 
+
 intro = '''
-    Intro will go here. You're practicing your instrument in music school and a hot guy catches you play. He's impressed and asks you what your name is.
-    ''' #Explain who you are, practicing, hot guy comes up and asks you what your name is
+    You're practicing your instrument in music school when a hot guy catches you play. He's impressed and asks you what your name is.
+    ''' 
 
 invite_to_practice = f'''
     'Well, I like your stuff. What do you think about coming over to my band's next practice to jam?'
-    ''' #An explination of what happens next, he invites you over to meet the band, you choose yes or no
+    ''' 
 
 realization = '''
     You realize who you're talking to. Reality shifts and everything comes into focus as you find yourself conversing with the one and only Griffen R., the hottest, most talented, most popular guitarist in the school's most beloved band. 
     '''
+
+band_practice = '''
+    \nIf you were a pirate you would certaintly be shivering your timbers, because as you walk through the front door and enter the foyee of Griffin R's... let's say, house? you are greeted by a cluster of very beautiful people. You feel the weight of your instrument case overcome your fragile, musical hands and hear the horrendous sound of your own dignity crash to the floor as you drop your case. THUD - that's it; you're a goner. Nobody will probably ever talk to you again. However, just as you begin to devise your escape, a pair of finely manicured hands flash into your vision, accompanied by your aforementioned Judas of a case. Looking up, you see who they belong to.\n\n'Hi', he says, 'you dropped your... uh, sorry...  what do you play again?
+'''
+
+andy_description = '''
+    He smiles, stealing your breath away, because he's left you no choice but to look at his magnificent face. Delicate, faded freckles speckle his face, shadowed by brilliant blades of bright red hair. Proportions nothing short of perfect; this dude certaintly is symetrical. 
+'''
+
+house_description = '''
+ "House" is a little too back-alley of a word to describe the place you find yourself in. Lathered in deep red carpetry, a tone that complements Andy's hair, and milky white walls with deep green velvety furniture, you're scared even breathing will soil the place's solemnity. At least, your breathing will. It isn't just Andy and Griffin who's been blessed with the attractive-gene in this room. There are still two more demi-gods your neurons are struggling to process. 
+'''
