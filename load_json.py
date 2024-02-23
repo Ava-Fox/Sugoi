e97f17dbbf4ec18036bc63883f8f8a16f973cc94
avafox <avafoxemail@gmail.com>
2024-02-23 15:25:29 -0500

    JSON data format mini txt-adv

--- load_json.py
+++ load_json.py
@@ -3,4 +3,13 @@ import json
 with open("sample.json", "r") as file:
     data = json.loads(file.read())
     print(data)
-    for thing in data
+    start = data['1']
+    while True:
+        print(start['prompt'])
+        answer = input("> ")
+        if answer in start['answers']:
+            if answer == "Goodbye":
+                break
+            next_start = start['answers'][answer]
+            print(next_start)
+            start = data[str(next_start)]
